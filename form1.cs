33cf9c39408e01c061a4bb2dce9c8061dbafe241
patrycze <patryk.pazura@gmail.com>
2017-02-15 20:34:46 +0100

    Update form1.cs

--- form1.cs
+++ form1.cs
@@ -19,21 +19,28 @@ namespace tastTest
             InitializeComponent();
         }
 
-        string s = String.Empty;
-        double temp, avarage = 0;
-        int splitBy = 0;
-         List<List<string>> values = new List<List<string>>();
          
         void loadFile_Click(object sender, EventArgs e)
         {
+            string s = String.Empty;
+            double temp = 0;
+            double avarage = 0;
+            int splitBy = 0;
+            List<List<string>> values = new List<List<string>>();
+
             using (StreamReader sr = File.OpenText("WIG20.txt"))
             {
                 while ((s = sr.ReadLine()) != null)
                 {
-                    List<string> values = new List<string>(s.Split(','));
-                   
+                    List<string> value = new List<string>(s.Split(','));
+
                     try
                     {
+                        if(splitBy > 15)
+                        {
+                            temp = 0;
+                            splitBy = 0;
+                        }
                         temp += double.Parse(value[5], CultureInfo.InvariantCulture);
                         avarage = temp / ++splitBy;
                         value.Add(avarage.ToString(CultureInfo.InvariantCulture));
@@ -41,19 +48,30 @@ namespace tastTest
                     }
                     catch (FormatException)
                     {
-                      
-                    }
-                    
-                    using (StreamWriter sw = new StreamWriter("WIG_001.txt"))
-                    {
 
-                        sw.WriteLine(s);
-                        foreach (List<string> Value in values)
-                        {
-                            sw.WriteLine(value);
-                        }
                     }
                 }
+
+                    int i = 0;
+                    string path = "WIG_" + i + ".txt";
+
+                while(File.Exists(path))
+                    {
+                    i++;
+                    path = "WIG_" + i + ".txt";
+                    }
+                using (StreamWriter sw = new StreamWriter(path))
+                    {
+
+                        //sw.WriteLine(s);
+                        foreach (List<string> Value in values)
+                        {
+                        Value.ForEach(x => sw.Write("{0}\t", x));
+                        sw.WriteLine("");
+                        }
+                    }
+               
+
             }
         }
 
